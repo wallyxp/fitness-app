b479fdd3539ca98d1c27373b8a2343a96cc723fc
Witty Wayne <wallycodes.noreply@gmail.com>
2022-07-04 03:13:28 +0530

    done till pagination setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
-import {Box} from '@mui/material'
+import Box from '@mui/material/Box'
 import './app.css'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
@@ -9,7 +9,7 @@ import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <Box width='400px' sx={{width: {xl: '1488px'}}} m="auto">
+    <Box width="400px" sx={{width: {xl: '1488px'}}} m="auto">
         <Navbar/>
         <Routes>
             <Route path='/' element={<Home/>}/>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,21 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import Box from '@mui/material/Box'
+import HeroBanner from '../components/HeroBanner'
+import SearchExercises from '../components/SearchExercises'
+import Exercises from '../components/Exercises'
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState('all')
+  const [exercises, setExercises] = useState([])
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner/>
+      <SearchExercises setExercises={setExercises}
+      bodyPart={bodyPart} setBodyPart={setBodyPart}/>
+      <Exercises
+      setExercises={setExercises}
+      bodyPart={bodyPart} exercises={exercises}
+      />
+    </Box>
   )
 }
 
