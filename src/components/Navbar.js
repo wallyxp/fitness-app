f9dcd7b8e533dc7dbd142ae43c36d4a95eb2b1ed
Witty Wayne <wallycodes.noreply@gmail.com>
2022-07-03 16:27:00 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import {Stack} from '@mui/icons-material'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
